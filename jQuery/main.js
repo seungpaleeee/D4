55e495c78543ee26dd55a21fbfc6751b6a587535
seungpaleeee <xorwlsdl12@naver.com>
2024-02-13 11:49:15 +0900

    리빙샵 완성

---
+++ jQuery/main.js
@@ -0,0 +1,43 @@
+$(function(){
+
+    //메뉴
+
+    $(".main > li").mouseover(function(){
+        $(".sub").stop().show();
+    })
+
+    $(".main > li").mouseout(function(){
+        $(".sub").stop().hide();
+    })
+
+    //슬라이드
+
+    $(".fade li").eq(0).siblings().hide();
+
+    var n = 0;//0 1 2
+
+    setInterval(function(){
+        if(n == 2){
+            n=0;
+        }else{
+            n++;
+        }
+
+        $(".fade li").eq(n).siblings().fadeOut();
+        $(".fade li").eq(n).fadeIn();
+
+    }, 2500)
+
+    //팝
+    $(".p_click").click(function(){
+
+        $(".pop").show();
+
+    })
+    $(".close").click(function(){
+
+        $(".pop").hide();
+
+    })
+
+})
